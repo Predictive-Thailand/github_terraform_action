be99a2da795437dc490ded9afbbca21e20654f8f
jitkasem <jitkasem@reseller.predictive.co.th>
2021-07-30 11:41:25 +0700

    modified version2

--- modules/gce/script.sh
+++ modules/gce/script.sh
@@ -3,4 +3,5 @@
 sudo google_metadata_script_runner --script-type startup
 sudo apt update -y; sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo apt install nginx -y
+sudo apt install wget -y
 sudo ufw allow 'Nginx HTTP'
